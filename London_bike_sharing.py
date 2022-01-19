9a11be297297275ab05649c5826fa11f8cda98ca
Jin-Woo Kim <dnehd6739@naver.com>
2022-01-20 00:48:41 +0900

    kaggle London_bike_sharing

--- London_bike_sharing.py
+++ London_bike_sharing.py
@@ -3,10 +3,20 @@ import pandas as pd
 import matplotlib.pyplot as plt # 그래프 작업을 위함
 import seaborn as sns # 시각화
 import missingno as msno # 결측치를 시각화
+
 from sklearn.model_selection import train_test_split # 훈련용 데이터, 테스트 데이터 분리
+
 from keras.models import Sequential
 from keras.layers import Dense
 from keras.callbacks import EarlyStopping # 학습중 과적합 잡아주는 라이브러리
+
+from sklearn.metrics import mean_squared_error # mse (실제값과 예측값의 차이를 구해주는 라이브러리)
+from sklearn.ensemble import RandomForestRegressor
+from xgboost import XGBRegressor
+from lightgbm import LGBMRegressor
+
+def RMSE(y_test,y_predict): # mse의 루트를 씌운모양(평균 제곱근 편차)
+    return np.sqrt(mean_squared_error(y_test,y_predict))
 
 def is_outliners(s):# 아웃라이어 제거(시그마 이상치 제거) 모든 데이터에는 잡음이 존재함 그것을 제거하기 위한 함수
     lower_limit = s.mean() - (s.std()*3) # 양 극단 값을 이상치로 추정하여 제거
@@ -43,16 +53,16 @@ def analysis():
     print()
     print(df['weather_code'].value_counts())  # 코드에 따른 의미가 있음.
 
-    a,b = plt.subplots(1,1,figsize=(10,5)) # 그래프 그리기
-    sns.boxplot(df['year'],df['cnt']) # 가로축, 세로축 년도별 이용횟수 (디버그 형태로 실행해야 보임)
+    # a,b = plt.subplots(1,1,figsize=(10,5)) # 그래프 그리기
+    # sns.boxplot(df['year'],df['cnt']) # 가로축, 세로축 년도별 이용횟수 (디버그 형태로 실행해야 보임)
 
-    a, b = plt.subplots(1, 1, figsize=(10, 5))  # 그래프 그리기
-    sns.boxplot(df['month'], df['cnt'])  # 가로축, 세로축 월별 이용횟수 (디버그 형태로 실행해야 보임)
+    # a, b = plt.subplots(1, 1, figsize=(10, 5))  # 그래프 그리기
+    # sns.boxplot(df['month'], df['cnt'])  # 가로축, 세로축 월별 이용횟수 (디버그 형태로 실행해야 보임)
 
-    a, b = plt.subplots(1, 1, figsize=(10, 5))  # 그래프 그리기
-    sns.boxplot(df['dayofweek'], df['cnt'])  # 가로축, 세로축 요일별 이용횟수 (디버그 형태로 실행해야 보임)
+    # a, b = plt.subplots(1, 1, figsize=(10, 5))  # 그래프 그리기
+    # sns.boxplot(df['dayofweek'], df['cnt'])  # 가로축, 세로축 요일별 이용횟수 (디버그 형태로 실행해야 보임)
 
-    plot_bar(df,'hour') # 시간에 따른 그래프 그리기
+    #plot_bar(df,'hour') # 시간에 따른 그래프 그리기
     df_out = df[~df.groupby('hour')['cnt'].apply(is_outliners)]
 
     print("이상치 제거전:",df.shape)
@@ -90,7 +100,7 @@ def analysis():
     print("X_test의 구조는:",X_test.shape) # 데이터 확인
     print("y_test의 구조는:",y_test.shape) # 데이터 확인
 
-    model = Sequential() # 계층형 학습법
+    model = Sequential() # 딥러닝 계층형 학습법
     model.add(Dense(units=160,activation='relu',input_dim=57))
     model.add(Dense(units=60,activation='relu'))
     model.add(Dense(units=20, activation='relu'))
@@ -100,9 +110,50 @@ def analysis():
 
     model.compile(loss='mae',optimizer='adam',metrics=['mae'])
     early_stop = EarlyStopping(monitor='loss',patience=5,mode='min')# 과적합이 될때 loss가 5번이 올라가면 멈춰야함
-    history = model.fit(X_train,y_train,epochs=50,batch_size=1,validation_split=0.1,callbacks=[early_stop]) # 학습한 모형 넣기
+   # history = model.fit(X_train,y_train,epochs=5,batch_size=1,validation_split=0.1,callbacks=[early_stop]) # 학습한 모형 넣기
     # validation_split은 검증용 데이터 훈련이 잘되고 있는지 확인하기 위함, 과적합 방지
-    
+
+    #plt.plot(history.history['val_loss'])
+    #plt.plot(history.history['loss'])
+    plt.title("loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("loss")
+    plt.legend(['val_loss','loss'])
+    plt.show()
+
+    y_predict = model.predict(X_test)
+    # 머신러닝 각 알고리즘 별 성능 비교
+
+    rf = RandomForestRegressor(n_estimators=100,random_state=16)
+    rf.fit(X_train,y_train)
+    rf_result = rf.predict(X_test)
+    print("RMSE:", RMSE(y_test, rf_result))  # 랜덤 포레스트를 거친후 RMSE
+
+    xgb = XGBRegressor(n_estimators=100,random_state=16)
+    xgb.fit(X_train,y_train)
+    xgb_result = xgb.predict(X_test)
+    print("RMSE:", RMSE(y_test, xgb_result))  # XGBRegressor를 거친후 RMSE
+
+    lgb = LGBMRegressor(n_estimators=100,random_state=16)
+    lgb.fit(X_train,y_train)
+    lgb_result = lgb.predict(X_test)
+    print("RMSE:", RMSE(y_test, lgb_result))  # LGBMRegressor를 거친후 RMSE
+
+    xgb = pd.DataFrame(xgb_result)
+    rf = pd.DataFrame(rf_result)
+    lgb = pd.DataFrame(lgb_result)
+    compare = pd.DataFrame(y_test).reset_index(drop=True)
+
+    compare['xgb'] = xgb
+    compare['rf'] = rf
+    compare['lgb'] = lgb
+    print(compare.head())
+
+    sns.kdeplot(compare['cnt'],shade=True,color='r')
+    sns.kdeplot(compare['xgb'],shade=True,color='b')
+    sns.kdeplot(compare['rf'],shade=True,color='y')
+    sns.kdeplot(compare['lgb'],shade=True,color='g')
+
 
 class exec:
     def __init__(self):
